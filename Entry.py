6af9840c936b272ce0e3849d4fdce54b606df966
Duckmadeofwood <122409597+Duckmadeofwood@users.noreply.github.com>
2023-01-10 16:34:52 -0700

    Add files via upload

---
+++ Entry.py
@@ -0,0 +1,11 @@
+def userEntry():
+    
+    header=("Traductor fonético español")
+    print(header)
+   
+    print("~" * 35)
+    entry = input("Por favor ingrese una palabra\n")
+
+
+
+userEntry()
